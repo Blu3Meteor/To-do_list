5c90038ffbec5c99d32e8cc5bdb75346a64d6d0d
Dron Dasgupta <71334148+Blu3Meteor@users.noreply.github.com>
2022-07-23 12:20:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -38,5 +38,5 @@ app.post("/", function(req, res) {
 })
 
 app.listen(process.env.PORT || 3000, function() {
-  console.log("Server is listening on port 3000");
+  console.log("Server is listening");
 });
